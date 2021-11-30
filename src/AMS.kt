7335bcb5624aefe55839bbd469b536c361c8e70f
anneconover <anne.conover@onepeloton.com>
2021-11-30 18:57:07 -0500

    getFortuneCookie()

--- src/AMS.kt
+++ src/AMS.kt
@@ -3,6 +3,8 @@ import java.util.*
 fun main(args: Array<String>) {
     print("Hello, ${args[0]}!")
     feedTheFish()
+    val fortune = getFortuneCookie()
+    println("Your fortune is: $fortune")
 }
 
 fun feedTheFish() {
@@ -14,4 +16,18 @@ fun feedTheFish() {
 fun randomDay() : String {
     val week = listOf ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     return week[Random().nextInt(7)]
+}
+
+fun getFortuneCookie() : String {
+    val fortunes = listOf ("You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
+        "Treasure your friends because they are your greatest fortune.")
+    println("Enter your birthday: ")
+    var birthday = readLine()!!.toIntOrNull() ?: 1
+    val num = birthday % fortunes.size
+    return fortunes.elementAt(num)
 }
