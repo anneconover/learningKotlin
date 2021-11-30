8fd1a72d97663b464867ef281859427dff8ffa14
anneconover <anne.conover@onepeloton.com>
2021-11-30 16:13:41 -0500

    greetings kotlin

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,3 +1,7 @@
+//fun main(args: Array<String>) {
+//    print("Hello, ${args[0]}!")
+//}
+
 fun main(args: Array<String>) {
-    print("Hello, World!")
+    println("${if (args[0].toInt() < 12) "Good morning, Kotlin" else "Good night, Kotlin"}")
 }
